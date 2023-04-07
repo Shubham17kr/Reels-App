7114533b12e7dd4c5b0acf055ce581181ae2c17d
Shubham17kr <shubhamkumar97423@gmail.com>
2023-04-07 15:54:09 +0530

    login sample

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
+
 function Login()
 {
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+
+    const trackEmail=function(e)
+    {
+        setEmail(e.target.value)
+    }
+    const trackpassword=function(e)
+    {
+        setPassword(e.target.value)
+    }
+    const printResult=()=>{
+        alert(email+" "+password)
+    }
+
     return(
         <>
-        login</>
+        <input type="email"  onChange={trackEmail} placeholder="Email"/><br/>
+        <input type="password"  onChange={trackpassword} placeholder="password"/><br/>
+        <button type="click" onClick={printResult} >Login</button>
+        </>
     )
 }
 export default Login
