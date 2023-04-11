8163da19d9b15f116c5b472129a8379fa6989f62
Shubham17kr <shubhamkumar97423@gmail.com>
2023-04-11 15:15:16 +0530

    signIn and SignOut functionality added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import {auth} from '../firebase'
-import {signInWithEmailAndPassword} from 'firebase/auth'
+import {signInWithEmailAndPassword,signOut} from 'firebase/auth'
 function Login()
 {
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
+    const [user,setUser]=useState(null)
+    const [load,setLoad]=useState(false)
+    const[error,setError]=useState("")
 
     const trackEmail=function(e)
     {
@@ -16,16 +19,42 @@ function Login()
     }
     const printResult= async function(){
         
-        let userCredential=await signInWithEmailAndPassword(auth,email,password)
+        try
+        {
+           setLoad(true)
+           let userCredential=await signInWithEmailAndPassword(auth,email,password)
+           setUser(userCredential.user)
 
-        console.log(userCredential.user)
+        }
+        catch(err)
+        {
+            setError(err.message)
+            setTimeout(()=>{
+                setError("")
+            },2000)
+
+        }
+        setLoad(false)
+    }
+
+    const logOut=async function()
+    {
+        await signOut(auth)
+        setUser(null)
     }
 
     return(
         <>
+        {
+            error != "" ?<h1>error is {error}</h1>:
+            load!=false?<h1>......Loading</h1>:
+            user != null? <><h1>user is {user.uid}</h1> <button onClick={logOut}>LogOut</button></>:
+        <>
         <input type="email"  onChange={trackEmail} placeholder="Email"/><br/>
         <input type="password"  onChange={trackpassword} placeholder="password"/><br/>
         <button type="click" onClick={printResult} >Login</button>
+        </>
+        }
         </>
     )
 }
