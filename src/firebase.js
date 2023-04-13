9e3401c50962f3a8ff44a3fc1fa267c4acac0e27
Shubham17kr <shubhamkumar97423@gmail.com>
2023-04-13 11:57:23 +0530

    hide API

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,12 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
-
+import firebaseConfig from "./secret";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDUmrBJ6onCSzeHLHzHwUc7kLa9vml6sek",
-  authDomain: "class-demo-4c843.firebaseapp.com",
-  projectId: "class-demo-4c843",
-  storageBucket: "class-demo-4c843.appspot.com",
-  messagingSenderId: "185485652041",
-  appId: "1:185485652041:web:58de58a040a50c6064cf98"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
