bac499d5e30311b3b4421d171897737c7ddb73f0
Shubham17kr <shubhamkumar97423@gmail.com>
2023-04-13 19:47:07 +0530

    profile page sample

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,21 @@
+import './profile.css'
+
 function Profile()
 {
     return(
         <>
-        Profile</>
+        <div className='header'></div>
+        <div className="main">
+            <div className="pimg_container">
+                <img className='pimg' src='https://via.placeholder.com/640x360' alt=''/>
+            </div>
+            <div className="details">
+                <div className="content">Shubham</div>
+                <div className="content">No. Of posts <span className='bold_text'>post</span></div>
+                <div className="content">Email <span className='bold_text'>email</span></div>
+            </div>
+        </div>
+        </>
     )
 }
 export default Profile
