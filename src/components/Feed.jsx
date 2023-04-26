652d90c962111758500bcc366c066581ec306d45
Shubham17kr <shubhamkumar97423@gmail.com>
2023-04-26 14:48:30 +0530

    feed component sample

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
+import "./Feed.css"
+
 function Feed()
 {
     return(
         <>
-        feed
+            <div className="header">
+                <img className="insta_img" src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png" alt=""></img> 
+                <img  className="profile_img" src="https://marketplace.canva.com/EAFXS8-cvyQ/1/0/1600w/canva-brown-and-light-brown%2C-circle-framed-instagram-profile-picture-2PE9qJLmPac.jpg" alt="" />
+            </div>
+            <div className="main_container">
+                <div className="upload_container">upload</div>
+                <div className="reel_container">reels</div>
+            </div>
         </>
     )
 }
